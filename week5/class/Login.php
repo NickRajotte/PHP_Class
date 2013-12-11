9ace0e6bbce2a571a6a1485603630a6c7540b10e
Week4 <nmrajotte@email.neit.edu>
2013-12-11 13:21:22 -0500

    Update Login.php

--- week5/class/Login.php
+++ week5/class/Login.php
@@ -9,26 +9,27 @@
 /**
  * Description of Login
  *
- * @author gforti
+ * @author GFORTI
  */
-class Login extends DB {
+class Login {
     //put your code here
-        
-    public function loginIsValid( $username, $password ) {
-        
-        $password = sha1($password);
-        $db = $this->getDB();
-        if ( NULL != $db ) {
-            $stmt = $db->prepare('select * from signup where username = :usernameValue and password = :passwordValue limit 1');
-            $stmt->bindParam(':usernameValue', $username, PDO::PARAM_STR);
-            $stmt->bindParam(':passwordValue', $password, PDO::PARAM_STR);
-            $stmt->execute();
-            $result = $stmt->fetch(PDO::FETCH_ASSOC);
-            
-            if ( is_array($result) && count($result) ) return true;
+    
+    public static function processLogin(){ 
+        if( $_SESSION["allowGuestbookAccess"] = false){
+             header("Location:login.php");  
         }
-        return false;
+                 
+        else if ( isset($_POST["passcode"]) && $_POST["passcode"] == "test" ) {
+            $_SESSION["allowGuestbookAccess"] = true;
+            header("Location:guestbook.php");                
+        }
     }
     
+    public static function confirmAccess() {
+        if ( ! isset($_SESSION["allowGuestbookAccess"]) 
+                || $_SESSION["allowGuestbookAccess"] != true ) {
+            header("Location:login.php");
+        }
+    }
     
 }
